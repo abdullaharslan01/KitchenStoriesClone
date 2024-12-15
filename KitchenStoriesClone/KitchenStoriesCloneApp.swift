055f01b61b8a6ca173466d778fc519b0a9a06ec7
Abdullah <abdullaharslan1473@gmail.com>
2024-12-15 19:06:45 +0300

    Initial Commit

---
+++ KitchenStoriesClone/KitchenStoriesCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenStoriesCloneApp.swift
+//  KitchenStoriesClone
+//
+//  Created by abdullah on 15.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct KitchenStoriesCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
